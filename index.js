3c64a012a5f422e42db800001b1100c911e48f6c
sreyaarc <sreya66nambiar@gmail.com>
2023-06-09 10:33:07 +0530

    implementing passport strategy for authentication with Google using the OAuth 2.0 API and handling the post request on delete

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import dotenv from "dotenv";
 import session from "express-session";
 import passport from "passport";
 import passportLocalMongoose from "passport-local-mongoose";
+import {Strategy as GoogleStrategy} from "passport-google-oauth20";
+import findOrCreate from "mongoose-findorcreate";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -51,16 +53,41 @@ const Blog = mongoose.model("Blog", blogSchema);
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
+    googleId: String,
     blogs : [blogSchema]
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// serialize and deserialize users locally :-
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
+// serialize and deserialize users authenticated with all types of strategies:-
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+
+passport.use(new GoogleStrategy ({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/home"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        // console.log(profile); // shows the entire profile details of the user
+        User.findOrCreate({ googleId: profile.id, username: profile.emails[0].value}, function (err, user) {  // if user not found already, creating a user with googleId and googleEmailId
+            return cb(err, user);
+        });
+    }
+));
 
 
 
@@ -86,6 +113,12 @@ app.get("/", (req, res) => {
     }  
 });
 
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/home', passport.authenticate('google', { failureRedirect: '/login' }), (req,res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
 
 app.get("/compose", (req, res) => {
     if(req.isAuthenticated()) {
@@ -96,7 +129,7 @@ app.get("/compose", (req, res) => {
     } else {
         res.redirect("/login")
     }
-})
+});
 
 app.post("/compose", (req, res) => {
     // console.log(req.user);
@@ -105,15 +138,26 @@ app.post("/compose", (req, res) => {
         title: req.body.title,
         content : req.body.content
     });
-    User.findByIdAndUpdate(req.user._id, {$push: {blogs: newBlog}}).then((updatedUser) => {
-        newBlog.save().then(() => {
-            console.log("saved into the collection")
-            res.redirect("/");
-        })  
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+    if(req.user.googleId != null) {
+        User.findByIdAndUpdate(req.user._id, {$push: {blogs: newBlog}}).then((updatedUser) => {
+            newBlog.save().then(() => {
+                console.log("saved into the collection")
+                res.redirect("/");
+            })  
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    } else {
+        User.findOneAndUpdate({username: req.user.username}, {$push: {blogs: newBlog}}).then((updatedUserByName) => {
+            newBlog.save().then(() => {
+                console.log("saved into the collection")
+                res.redirect("/");
+            })
+        }).catch((err) => {
+            console.log(err)
+        });
+    }
 });
 
 app.get("/blogs/:blogTitle", (req, res) => {
@@ -155,37 +199,56 @@ app.get("/blogs/:blogTitle", (req, res) => {
 });
 
 app.get("/ME", (req, res) => {
-    if(req.user) {
-        User.findById(req.user._id).then((foundUser) => {
-            console.log(foundUser.username)
-            if(foundUser) {
-                res.render("account", {
-                    logged: "LogOut",
-                    username: foundUser.username,
-                    docs: foundUser.blogs
-                });
-            } else {
-                res.redirect("/login")
-            }    
-        }).catch((err) => {
-            console.log(err)
-        })
-    } else {
+    // console.log("req.user = ", req.user)
+    if(req.user) {  // if the user is defined
+        if((req.user.googleId != null)) {  // if the user is authenticated by google, then the user will have a googleId
+            User.findById(req.user._id).then((foundUser) => {
+                // console.log("foundUser = "+foundUser)
+                if(foundUser) {
+                    res.render("account", {
+                        logged: "LogOut",
+                        username: foundUser.username,
+                        docs: foundUser.blogs
+                    });
+                } else {
+                    res.redirect("/login")
+                }    
+            }).catch((err) => {
+                console.log(err)
+            });
+        } else if(req.user.googleId == null) {  // whereas users registered using local strategy(by logging in manually using their emailId) wont have a googleId, hence finding them using username
+            User.findOne({username: req.user.username}).then((foundUserByName) => {
+                // console.log("foundUserByName = "+foundUserByName)
+                if(foundUserByName) {
+                    res.render("account", {
+                        logged: "LogOut",
+                        username: foundUserByName.username,
+                        docs: foundUserByName.blogs
+                    });
+                } else {
+                    res.redirect("/login")
+                }  
+            }).catch((err) => {
+                console.log(err)
+            });
+        }
+    } else {  //if the user is undefined
         res.redirect("/login")
     }
 });
+
 
 app.get("/register", (req, res) => {
     if(req.isAuthenticated()) {
         res.render("register", {
             isRegistered: "ME",
             logged: "LogOut"
-        })
+        });
     } else {
         res.render("register", {
             isRegistered: "Register",
             logged: "Login"
-        })
+        });
     }
     
 });
@@ -194,7 +257,7 @@ app.get("/Login", (req, res) => {
     res.render("login", {
         isRegistered: "Register",
         logged: "Login"
-    }) 
+    }); 
 });
 
 app.get("/LogOut", (req, res) => {
@@ -204,12 +267,12 @@ app.get("/LogOut", (req, res) => {
         } else {
             res.redirect("/");
         }
-    })
-})
+    });
+});
 
 app.post("/register", (req, res) => {
     // using passport-local-mongoose to register the user
-    // console.log(req.body)
+    console.log("register details = " + req.body)
     if(req.body.password === req.body.confirmPassword) {
         User.register({username: req.body.username}, req.body.password, function(err, user) {
             if(err) {
@@ -217,9 +280,8 @@ app.post("/register", (req, res) => {
                 res.redirect("/register");
             } else {
                 // console.log(user)
-                console.log("request="+req)
+                // console.log("request=" + req)
                 passport.authenticate("local")(req, res, function() {
-                    
                     res.redirect("/login");
                 });
             };
@@ -238,27 +300,44 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/Login", passport.authenticate('local', {failureRedirect: "/login"}), function(req,res) {
-    const newUser = new User({
-        username: req.body.username,
-        password: req.body.password
+    User.findOne({username: req.body.username}).then((foundUser) => {
+        const newUser = new User({
+            username: req.body.username,
+            password: req.body.password,
+            blogs: foundUser.blogs
+        });
+        req.login(newUser, function(err) {
+            if(err) {
+                console.log(err);
+                res.redirect("/login");
+            } else {
+                res.redirect("/");
+            }
+        });
     });
-    req.login(newUser, function(err) {
-        if(err) {
-            console.log(err);
-            res.redirect("/login");
-        } else {
-            res.redirect("/");
-        }
-    })
+});
 
-})
+app.post("/delete", (req, res) => {
+    const deleteId = req.body.deleteId;
+    // console.log("doc = ", deleteId);
+    // console.log("req.user = ",req.user)
+
+    Blog.findByIdAndDelete(deleteId).then((doc) => {  // deleting from the home page
+        // console.log(doc)
+        console.log("deleted from home page");
+        res.redirect("/ME");
+    }).catch((e) => console.log(e));
+
+    const query = {username: req.user.username};
+    User.findOneAndUpdate(query, {$pull: {blogs: {_id: deleteId}}}).then(() => {  // deleting from the user account
+        console.log("deleted from the user");
+        res.redirect("/ME");
+    }).catch((e) => console.log(e));
+});
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Server started at port "+port);
-})
-
-
-
-// demo : demo@user.com
-// pwd : demouserdemo
+});
