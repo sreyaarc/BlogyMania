d7cb53c517e31f22f8f749365fc7328738abb02e
sreyaarc <sreya66nambiar@gmail.com>
2023-06-09 20:39:09 +0530

    implement javascript for making a responsive navbar

--- javascript/toggle.js
+++ javascript/toggle.js
@@ -1,10 +1,24 @@
-// const toggleBtn = document.querySelector(".toggle");
-// toggleBtn.addEventListener("click", (e) => {
-//     const contents = toggleBtn.parentElement.children;
-//     // console.log(contents)
-//     contents.className = "contentToggle";
+
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", (e) => {
+    const subNav = document.querySelector(".subNav");
+    const newNavbar = document.querySelector(".navbar");
+    const container = document.querySelector(".container");  // home page
+    const newForm = document.querySelectorAll(".newForm");   // login and register page
+    const username = document.querySelector(".username");    // account page
+
+    // check if an element is successfully selected using querySelector before trying to access its classList. 
+    // The ?. operator is used to conditionally access the classList property only if the element is successfully selected. 
+    // This ensures that the code doesn't throw an error if any of the elements are not present in the DOM at the time the code runs.
+
+    subNav?.classList.toggle("active");
+    newNavbar?.classList.toggle("new");
+    container?.classList.toggle("newCont");
+    newForm?.[0]?.classList.toggle("formMargin");
+    username?.classList.toggle("newUser");
     
-// })
+});
+
 
 const navbar = document.querySelector(".navbar");
 window.onscroll = () => {
